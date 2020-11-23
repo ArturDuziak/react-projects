dcdca8e8acc2a30f8150c4bfe9d287a64cb9a8c7
Artur Duziak <artur.duziak@netguru.com>
2020-11-23 17:25:03 +0100

    Add simple error page
    Added simple error page that links back to home page

--- src/components/error-page/ErrorPage.js
+++ src/components/error-page/ErrorPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BiErrorAlt } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 export const ErrorPage = () => {
   return (
-    <div>404 page not found</div>
-  )
+    <div className="error-page-container">
+      <BiErrorAlt className="error-icon" />
+
+      <span>Page not found...</span>
+      <Link to="/" className="btn">
+        Go back home
+      </Link>
+    </div>
+  );
 };
