f2584f3d1c4bccf78e4a3955fc00948e05962709
Artur Duziak <artur.duziak@netguru.com>
2020-11-25 21:16:10 +0100

    Close navbar when clicking outside of it
    Now navbar should be closed after user clicks outside of it

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,39 +9,45 @@ export const Navbar = () => {
   const { isSidebarDisplayed, closeSidebar } = useGlobalContext();
 
   return (
-    <aside className={`sidebar ${isSidebarDisplayed && "show-sidebar"}`}>
-      <div className="sidebar-header">
-        <img src={logo} className="logo" alt="website logo" />
-        <button className="close-btn" onClick={closeSidebar}>
-          <FaTimes />
-        </button>
-      </div>
-      <ul className="links">
-        {pages.map(item => {
-          const { id, url, text, icon } = item;
-          return (
-            <li key={id}>
-              <a href={url}>
-                {icon}
-                {text}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <span>Hit me up on</span>
-      <ul className="social-icons">
-        {social.map(item => {
-          const { id, url, icon } = item;
-          return (
-            <li key={id}>
-              <a href={url} target="_blank">
-                {icon}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </aside>
+    <>
+      <aside className={`sidebar ${isSidebarDisplayed && "show-sidebar"}`}>
+        <div className="sidebar-header">
+          <img src={logo} className="logo" alt="website logo" />
+          <button className="close-btn" onClick={closeSidebar}>
+            <FaTimes />
+          </button>
+        </div>
+        <ul className="links">
+          {pages.map(item => {
+            const { id, url, text, icon } = item;
+            return (
+              <li key={id}>
+                <a href={url}>
+                  {icon}
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <span>Hit me up on</span>
+        <ul className="social-icons">
+          {social.map(item => {
+            const { id, url, icon } = item;
+            return (
+              <li key={id}>
+                <a href={url} target="_blank">
+                  {icon}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </aside>
+      <aside
+        className={`sidebar-overlay ${isSidebarDisplayed && "show-overlay"}`}
+        onClick={closeSidebar}
+      ></aside>
+    </>
   );
 };
