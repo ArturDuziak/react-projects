5cd67c393b8ef52422ce839073512faeb6367ee1
Artur Duziak <artur.duziak@netguru.com>
2021-01-02 13:35:12 +0100

    Add up cypress to repo
    Added up cypress to the repository

---
+++ cypress/integration/ToDoApp/AddToDo.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe("Checks adding to do functions", () => {
+  it('', () => {
+    cy.visit('/todo-app');
+    cy.contains
+  })
+})
---
+++ cypress/integration/ToDoApp/DeleteToDo.spec.js
@@ -0,0 +1,6 @@
+describe("Checks deleteing TO DO", () => {
+  it("checks", () => {
+    cy.visit("/todo-app");
+    cy.get()
+  });
+});
